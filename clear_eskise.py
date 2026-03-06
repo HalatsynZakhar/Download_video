d8a36425c123ad3f3e015b06484dff8db5e467c4
HalatsynZakhar <halatsynzakhar@gmail.com>
2026-03-06 08:17:51 +0200

    fix

--- clear_eskise.py
+++ clear_eskise.py
@@ -1,14 +1,13 @@
 import os
 import glob
-import shutil
 import subprocess
+import shutil
 
 def delete_files(pattern):
-    files = glob.glob(pattern)
     deleted = 0
-    for f in files:
+    for file in glob.glob(pattern):
         try:
-            os.remove(f)
+            os.remove(file)
             deleted += 1
         except:
             pass
@@ -19,8 +18,9 @@ def clear_folder(folder):
     if os.path.exists(folder):
         for root, dirs, files in os.walk(folder):
             for name in files:
+                path = os.path.join(root, name)
                 try:
-                    os.remove(os.path.join(root, name))
+                    os.remove(path)
                     deleted += 1
                 except:
                     pass
@@ -31,35 +31,50 @@ subprocess.run("taskkill /f /im explorer.exe", shell=True)
 
 local = os.path.expandvars(r"%LocalAppData%")
 appdata = os.path.expandvars(r"%AppData%")
+programdata = os.path.expandvars(r"%ProgramData%")
 temp = os.path.expandvars(r"%TEMP%")
 
 deleted_total = 0
 
-# 1. Thumbnail cache
+print("Очистка thumbnail cache...")
 deleted_total += delete_files(local + r"\Microsoft\Windows\Explorer\thumbcache*.db")
 
-# 2. Icon cache
+print("Очистка icon cache...")
 deleted_total += delete_files(local + r"\Microsoft\Windows\Explorer\iconcache*.db")
 
-# 3. Explorer cache
+print("Очистка Explorer cache...")
 deleted_total += delete_files(local + r"\Microsoft\Windows\Explorer\*.db")
 
-# 4. Recent files
+print("Очистка Recent files...")
 deleted_total += clear_folder(appdata + r"\Microsoft\Windows\Recent")
 
-# 5. Jump Lists
+print("Очистка Jump Lists...")
 deleted_total += clear_folder(appdata + r"\Microsoft\Windows\Recent\AutomaticDestinations")
 deleted_total += clear_folder(appdata + r"\Microsoft\Windows\Recent\CustomDestinations")
 
-# 6. Temp user
+print("Очистка Windows Search index...")
+deleted_total += clear_folder(programdata + r"\Microsoft\Search\Data")
+
+print("Очистка TEMP пользователя...")
 deleted_total += clear_folder(temp)
 
-# 7. Windows temp
+print("Очистка Windows TEMP...")
 deleted_total += clear_folder(r"C:\Windows\Temp")
 
-# 8. Edge/Chrome image cache
+print("Очистка Prefetch...")
+deleted_total += clear_folder(r"C:\Windows\Prefetch")
+
+print("Очистка Edge cache...")
 deleted_total += clear_folder(local + r"\Microsoft\Edge\User Data\Default\Cache")
+
+print("Очистка Chrome cache...")
 deleted_total += clear_folder(local + r"\Google\Chrome\User Data\Default\Cache")
+
+print("Очистка DirectX shader cache...")
+deleted_total += clear_folder(local + r"\D3DSCache")
+
+print("Отключаем гибернацию (удаляет hiberfil.sys)...")
+subprocess.run("powercfg -h off", shell=True)
 
 print(f"Удалено файлов: {deleted_total}")
 
